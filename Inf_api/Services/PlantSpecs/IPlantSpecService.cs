ae8ed9995153fc3c30ea96e445f0dc0929b2c41e
DESKTOP-TH66SAT\Vahid <vakson12@gmail.com>
2024-10-25 01:59:08 +0200

    Add plant specifications service

---
+++ Inf_api/Services/PlantSpecs/IPlantSpecService.cs
@@ -0,0 +1,11 @@
+﻿using Inf_Data.Entities;
+using Inf_Transfer.utils;
+
+namespace Inf_api.Services.PlantSpecs
+{
+    public interface IPlantSpecService
+    {
+        Task<QueryResponse<PlantSpecifications>> GetAllPlantSpecs(string search,int pageSize=10, int page = 1);
+            
+            }
+}
---
+++ Inf_api/Services/PlantSpecs/PlantSpecService.cs
@@ -0,0 +1,46 @@
+﻿using Inf_api.Services.PlantSpecs;
+using Inf_Data.Entities;
+using Inf_Repository.Repository.PlantSpecs;
+using Inf_Transfer.utils;
+using Microsoft.EntityFrameworkCore;
+
+public class PlantSpecService : IPlantSpecService
+{
+    private readonly IPlantSpecificationRepository _plantSpecificationRepository;
+
+    public PlantSpecService(IPlantSpecificationRepository plantSpecificationRepository)
+    {
+        _plantSpecificationRepository = plantSpecificationRepository;
+    }
+
+    public async Task<QueryResponse<PlantSpecifications>> GetAllPlantSpecs(string search, int pageSize = 10, int page = 1)
+    {
+        var query =  _plantSpecificationRepository.GetAll();
+
+        // Apply search filter
+        if (!string.IsNullOrEmpty(search))
+        {
+            query = query.Where(plant => plant.SpecieName.Contains(search));
+        }
+
+        // Get the total count before applying paging
+        var totalCount = await query.CountAsync();
+
+        // Apply paging
+        query = query.Skip((page - 1) * pageSize).Take(pageSize);
+
+        // Execute the query and get the result
+        var plants = await query.ToListAsync();
+
+        // Prepare the result
+        var result = new QueryResponse<PlantSpecifications>()
+        {
+            Items = plants,
+            TotalCount = totalCount,
+            PageSize = pageSize,
+            CurrentPage = page
+        };
+
+        return result;
+    }
+}
