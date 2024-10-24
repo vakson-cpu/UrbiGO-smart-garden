004bc28af4c93be387de71592e5215f161782256
DESKTOP-TH66SAT\Vahid <vakson12@gmail.com>
2024-10-25 01:58:58 +0200

    Add plant service

---
+++ Inf_api/Services/Plant/IPlantService.cs
@@ -0,0 +1,13 @@
+﻿using Inf_Data.Entities;
+using Inf_Repository.Repository.Generic;
+using Inf_Transfer.DTOS.PlantsDtos;
+using Inf_Transfer.utils;
+
+namespace Inf_api.Services.Plant
+{
+    public interface IPlantService
+    {
+        Task<CustomResponse<Inf_Data.Entities.Plant>> AddPlant(PlantsDtos addPlant);
+        Task<CustomResponse<List<Inf_Data.Entities.Plant>>> GetAllPlants();
+    }
+}
---
+++ Inf_api/Services/Plant/PlantService.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Inf_api.Extensions;
+using Inf_Data.Entities;
+using Inf_Repository.Repository.UnitOfWork;
+using Inf_Transfer.DTOS.PlantsDtos;
+using Inf_Transfer.utils;
+
+namespace Inf_api.Services.Plant
+{
+    public class PlantService : IPlantService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PlantService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<CustomResponse<Inf_Data.Entities.Plant>> AddPlant(PlantsDtos addPlant)
+        {
+            var plant = new Inf_Data.Entities.Plant();
+
+            // Assuming CopyProperties is an extension method or a method to copy DTO properties to the entity
+            addPlant.CopyProperties(plant);
+
+            await _unitOfWork.PlantRepository.Create(plant);
+            await _unitOfWork.SaveChanges();
+
+            return new CustomResponse<Inf_Data.Entities.Plant>(
+                message: "Plant added successfully",
+                succeeded: true,
+                data: plant
+            );
+        }
+
+        public async Task<CustomResponse<List<Inf_Data.Entities.Plant>>> GetAllPlants()
+        {
+            var result = await _unitOfWork.PlantRepository.GetAllPlants();
+            var response = new CustomResponse<List<Inf_Data.Entities.Plant>>(
+                message: "Request succeeded",
+                succeeded: true,
+                data: result
+            );
+            return response;
+        }
+
+    }
+}
