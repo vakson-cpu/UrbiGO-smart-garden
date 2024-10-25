9f446ccf81ef7b48ba59bec4facf0ae1bf4d1f40
DESKTOP-TH66SAT\Vahid <vakson12@gmail.com>
2024-10-25 02:00:01 +0200

    Add unit of work

--- Inf_Repository/UnitOfWork/IUnitOfWork.cs
+++ Inf_Repository/UnitOfWork/IUnitOfWork.cs
@@ -1,11 +1,13 @@
+using Inf_Repository.Repository.Devices;
 using Inf_Repository.Repository.Generic;
+using Inf_Repository.Repository.Plant;
 using Inf_Repository.Repository.User;
 
 namespace Inf_Repository.Repository.UnitOfWork;
 public interface IUnitOfWork:IAsyncDisposable
 {
     IUserRepository UserRepository { get;set; }   
-    
-
+    IPlantRepository PlantRepository { get; set; }
+    IDeviceRepository DeviceRepository { get; set; }
     Task SaveChanges();   
 }
--- Inf_Repository/UnitOfWork/UnitOfWork.cs
+++ Inf_Repository/UnitOfWork/UnitOfWork.cs
@@ -1,23 +1,28 @@
 using Inf_Data;
+using Inf_Repository.Repository.Devices;
+using Inf_Repository.Repository.Plant;
+using Inf_Repository.Repository.PlantSpecs;
 using Inf_Repository.Repository.User;
 
 namespace Inf_Repository.Repository.UnitOfWork;
 
 public class UnitOfWork : IUnitOfWork
 {
-    public IUserRepository UserRepository { get; set; }
     public InfDbContext _context { get; set; }
+    public IPlantRepository PlantRepository { get; set; }
+    public IUserRepository UserRepository { get; set; }
+
+    public IDeviceRepository DeviceRepository { get; set; }
+    private readonly IPlantSpecificationRepository plantSpecificationRepository;
 
     public UnitOfWork(InfDbContext context)
     {
         this._context = context;
         this.UserRepository = new UserRepository(context);
+        this.PlantRepository = new PlantRepository(context);
+        this.DeviceRepository = new DeviceRepository(context);
+        this.plantSpecificationRepository = new PlantSpecificationRepostiory(context);
     }
-
-    // public async Task Dispose()
-    // {
-    //     await this._context.DisposeAsync();
-    // }
 
     public async Task SaveChanges() {
         await this._context.SaveChangesAsync();
