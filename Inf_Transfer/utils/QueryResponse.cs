a7f30d65854a46475c7b76b5f0a34f0c73e518e8
DESKTOP-TH66SAT\Vahid <vakson12@gmail.com>
2024-10-25 02:00:33 +0200

    Add generic query response

---
+++ Inf_Transfer/utils/QueryResponse.cs
@@ -0,0 +1,13 @@
+﻿using Inf_Data.Entities;
+
+namespace Inf_Transfer.utils
+{
+    public class QueryResponse<T>
+    {
+        public List<T> Items { get; set; } = new();
+        public int TotalCount { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+    }
+
+}
