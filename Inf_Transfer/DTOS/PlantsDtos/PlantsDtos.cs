515c919cd6c217632d8c66c5a4305db3ca93e76b
DESKTOP-TH66SAT\Vahid <vakson12@gmail.com>
2024-10-25 02:00:25 +0200

    hotfix

---
+++ Inf_Transfer/DTOS/PlantsDtos/PlantsDtos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inf_Transfer.DTOS.PlantsDtos
+{
+    public class PlantsDtos
+    {
+        public string LatinName { get; set; }
+        public string LocalName { get; set; }
+
+        public double CurrentWattering { get; set; }
+
+        public float CurrentTemperature { get; set; }
+
+        public float CurrentIllumination { get; set; }
+
+        public int PlantSpecificationId { get; set; }
+
+    }
+}
